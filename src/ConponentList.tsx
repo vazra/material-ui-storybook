88064d708895904ac1869c5d6d530cc051931bd6
Ameen Rashad <ameen.vazhayil@datafilabs.com>
2021-01-18 12:57:16 +0100

    feat: added contained-buttons

---
+++ src/ConponentList.tsx
@@ -0,0 +1,14 @@
+function ComponentList() {
+  return (
+    <div>
+      <dl>
+        <dt>Button</dt>
+        <dd>- Contained Buttons</dd>
+        <dt>Milk</dt>
+        <dd>- white cold drink</dd>
+      </dl>
+    </div>
+  )
+}
+
+export default ComponentList
