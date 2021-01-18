bb8469c4a4cb7c5d08e45ca72866037449aa52d7
Ameen Rashad <ameen.vazhayil@datafilabs.com>
2021-01-18 15:08:01 +0100

    feat: added component list

--- src/ConponentList.tsx
+++ src/ConponentList.tsx
@@ -1,12 +1,24 @@
+import { Grid } from '@material-ui/core'
+import React from 'react'
+import ButtonGroups from './components/ButtonGroups'
+import Buttons from './components/Buttons'
+// import Grids from './components/Grids'
+import Selects from './components/Selects'
+
 function ComponentList() {
   return (
     <div>
-      <dl>
-        <dt>Button</dt>
-        <dd>- Contained Buttons</dd>
-        <dt>Milk</dt>
-        <dd>- white cold drink</dd>
-      </dl>
+      <Grid container direction='column' justify='center' alignItems='stretch'>
+        <Grid item>
+          <Buttons />
+        </Grid>
+        <Grid item>
+          <ButtonGroups />
+        </Grid>
+        <Grid item>
+          <Selects />
+        </Grid>
+      </Grid>
     </div>
   )
 }
